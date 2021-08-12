a9184e9e2e6c8c8c89c87e00dad22fff9f2b1208
Engin Karataş <43602725+EnginKARATAS@users.noreply.github.com>
2021-08-12 09:24:19 +0300

    Module not found err

--- weekly-report-app/index.js
+++ weekly-report-app/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 var cors = require("cors");
-
 app.use(cors());
+
+app.use(express.json());
 
 let report = {
   data: {
--- weekly-report-app/src/app/models/reports.ts
+++ weekly-report-app/src/app/models/reports.ts
@@ -1,15 +1,16 @@
 export interface Report {
-   
-        id: number,
-        matter: number,
-        startDate: Date,
-        finishDate: Date,
-        workDone: string,
-        claimant: string,
-        scheduledCompletionDate: Date,
-        timeout: boolean,
-        weeklyTimeSpent: number,
-        status: string,
-        comments: string,
- 
+  workerId: number;
+  rows: [
+    id: number,
+    matter: number,
+    startDate: Date,
+    finishDate: Date,
+    workDone: string,
+    claimant: string,
+    scheduledCompletionDate: Date,
+    timeout: boolean,
+    weeklyTimeSpent: number,
+    status: string,
+    comments: string
+  ];
 }

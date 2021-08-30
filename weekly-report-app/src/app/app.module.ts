4ec891b6759c4c040da0beb084ef4f603417552a
Engin Karataş <43602725+EnginKARATAS@users.noreply.github.com>
2021-08-30 13:44:52 +0300

    gmadmin component moved to reports folder, gmlogin and login components moved to their own folder

--- weekly-report-app/src/app/app.module.ts
+++ weekly-report-app/src/app/app.module.ts
@@ -3,15 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
-import { ReportFormComponent } from './reports/report-form.component';
-import { ReportNotificationComponent } from './reports/report-notification/report-notification.component';
 import { NaviComponent } from './navi/navi.component';
 import { HttpClientModule } from '@angular/common/http';
 import { TodoComponent } from './todo/todo.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './login/worker-login/login.component';
+import { GmloginComponent } from './login/gm-login/gmlogin.component';
 import { VatAddedPipe } from './pipes/vat-added.pipe';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { EditWeeklyReportComponent } from './reports/edit-weekly-report/edit-weekly-report.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatSliderModule } from '@angular/material/slider';
@@ -23,9 +21,7 @@ import { ToastrModule } from 'ngx-toastr';
 
 import { CookieService } from 'ngx-cookie-service';
 import { GmadminComponent } from './gmadmin/gmadmin.component';
-import { LoginGuard } from './guards/login.guard';
 import { AppRoutingModule } from './app-routing.module';
-import { GmloginComponent } from './login/gmlogin.component';
 import { AddNewWorkerComponent } from './add-new-worker/add-new-worker.component';
 import { SetPasswordComponent } from './set-password/set-password.component';
 import { ReportModule } from './reports/report.module';

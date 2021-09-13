72a4dd63553f84ebe03f00a48d705055f6dea099
Engin Karataş <43602725+EnginKARATAS@users.noreply.github.com>
2021-09-13 08:32:56 +0300

    angular budget size increased

--- backend-weekly-report/src/middleware/checkAuth.js
+++ backend-weekly-report/src/middleware/checkAuth.js
@@ -1,19 +1,22 @@
-const jwt = require('jsonwebtoken');
+        const jwt = require("jsonwebtoken");
 
-module.exports = (req, res, next) => {
-    try {
-        /*JWT is send with request header! 
-        Format of it: Authorization : Bearer <token>
-        */
-        const token = req.headers.authorization;
-        const decodedToken = jwt.verify(token, 'dvurising');
-        req.userData = decodedToken;    
-        console.log("🚀 ~ file: checkAuth.js ~ line 11 ~ req.userData", req.userData)
-        next();
-    }catch(error) {
-        return res.json({
-            message: 'Doğrulanamayan kullanıcı. Lütfen giriş yapınız. ',
-            resCode: 401
-        });
-    }
-}
+        module.exports = (req, res, next) => {
+        try {
+            /*JWT is send with request header! 
+                Format of it: Authorization : Bearer <token>
+                */
+            const token = req.headers.authorization;
+            const decodedToken = jwt.verify(token, "dvurising");
+            req.userData = decodedToken;
+            console.log(
+            "🚀 ~ file: checkAuth.js ~ line 11 ~ req.userData",
+            req.userData
+            );
+            next();
+        } catch (error) {
+            return res.json({
+            message: "Doğrulanamayan kullanıcı. Lütfen giriş yapınız. ",
+            resCode: 401,
+            });
+        }
+        };
